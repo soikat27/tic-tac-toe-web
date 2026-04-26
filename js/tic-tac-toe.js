e264e01bc9fd643f264777fa6d7de4ad5429efb8
Soikat <193371447+soikat27@users.noreply.github.com>
2026-04-26 01:42:46 -0400

    Initialize project file structure and Implement gameBoard Object
    -- Setup html, add normalize.css and project-specific style files
    -- Implement gameBoard Object and test its functionality

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,28 @@
+/* GameBoard object */
+function gameBoard (row = 3, col = 3) {
+    const boardState = [];
+
+    function initializeGameBoard () {
+        for (let i = 0; i < row; i++) {
+            boardState[i] = [];
+            for (let j = 0; j < col; j++) {
+                boardState[i][j] = "";
+            }
+        }
+    }
+    initializeGameBoard();
+
+    function isEmpty (row, col) {
+        return boardState[row][col] === "";
+    }
+
+    function placeMove (row, col, token) {
+        boardState[row][col] = token;
+    }
+
+    function getBoardState () { 
+        return boardState;
+    }
+
+    return {isEmpty, placeMove, getBoardState};
+}
